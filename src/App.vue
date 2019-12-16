e71ee5592c86642665acacae7c74defc15f73d55
Pitarn Harnpanichpun <pharn@sertiscorp.com>
2019-12-16 18:30:10 +0700

    fix typo

--- src/App.vue
+++ src/App.vue
@@ -8,6 +8,16 @@
   </div>
 </template>
 
+<script>
+import Header from './components/Header'
+export default {
+  name: "app",
+  components: {
+    Header
+  }
+}
+</script>
+
 <style>
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
